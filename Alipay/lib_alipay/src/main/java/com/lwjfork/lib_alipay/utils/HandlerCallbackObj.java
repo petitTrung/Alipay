f459a1c62eb40bba8bd6655df2dac0ac78279b6f
liuwenjie <liuwenjie@goumin.com>
2015-08-10 14:42:31 +0800

    delete file

--- Alipay/lib_alipay/src/main/java/com/lwjfork/lib_alipay/utils/HandlerCallbackObj.java
+++
@@ -1,8 +0,0 @@
-package com.lwjfork.lib_alipay.utils;
-
-public class HandlerCallbackObj<T extends  OrderInfoModel> {
-	public String result;
-	public boolean isExist;
-	public T orderInfoModel;
-	public IAlipayListener alipayListener;
-}
